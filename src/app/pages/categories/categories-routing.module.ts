6b4f37b3669a8bf9e6f4b4c37f2f343d35d84f5a
Luan Trajano <luan.trajano@biz.com.br>
2022-02-08 22:01:46 -0300

    primeiras rotas

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,10 +1,16 @@
+import { CategoryListComponent } from './category-list/category-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CategoriesRoutingModule { }
+export class CategoriesRoutingModule {}
